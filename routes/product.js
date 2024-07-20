ae97bdb7730fbae98b8bf063a83d024d43d77093
fraavs <ftcinfraavs@gmail.com>
2024-07-21 01:34:02 +0900

    created keyword query.

--- routes/product.js
+++ routes/product.js
@@ -6,15 +6,20 @@ products.get('/', (req, res) => {
 
     var mainCategoryId = req.query.maincategoryid;
     var subCategoryId = req.query.subcategoryid;
+    var keyword = req.query.keyword;
 
     let query = 'select * from products';
     if (subCategoryId) {
-        query +=' where category_id = ' + subCategoryId;
+        query += ' where category_id = ' + subCategoryId;
     }
 
     if (mainCategoryId) {
         query = `select products.* from products, categories
         where products.category_id = categories.id and categories.parent_category_id = ${mainCategoryId}`;
+    }
+
+    if (keyword) { 
+        query += ` and keywords like '%${keyword}'`;
     }
 
     pool.query(query, (error, products) => {
