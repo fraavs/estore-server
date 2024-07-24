02a139322249750a658b2bc824c142317d96821b
fraavs <ftcinfraavs@gmail.com>
2024-07-24 21:25:10 +0900

    created signup.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const productCategories = require('./routes/productCategories');
 const products = require('./routes/product');
+const users = require('./routes/users');
 const cors = require('cors');
 const app = express();
 const PORT = 5001;
+const bodyparser = require('body-parser');
 
 app.use(cors());
-
+app.use(bodyparser.json());
 app.use('/productCategories', productCategories);
 app.use('/products', products);
+app.use('/users', users);
 
 const server = app.listen(5001, () => {
     console.log('App is running on the port-5001')
---
+++ routes/users.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const pool = require('../shared/pool');
+const bcryptjs = require('bcryptjs');
+const users = express.Router();
+
+
+users.post('/signup', (req, res) => {
+    try {
+        let username = req.body.username;
+        let email = req.body.email;
+        let firstName = req.body.firstName;
+        let lastName = req.body.lastName;
+        let password = req.body.password;
+
+        pool.query(`select count(*) as count from users where email like '${email}'`,
+            (error, resultCount) => {
+                if (error) {
+                    res.status(500).send(error);
+                } else {
+                    if (resultCount[0].count > 0) {
+                        res.status(200).send('Email already exists');
+                    } else {
+                        bcryptjs.hash(password, 10).then((hashPassword) => {
+                            const query = `Insert into users
+                            (email,firstName,lastName,username,password) values (
+                            '${email}','${firstName}','${lastName}','${username}','${hashPassword}')`;
+                            pool.query(query, (error, result) => {
+                                if (error) {
+                                    res.status(401).send(error);
+                                } else {
+                                    res.status(201).send('success');
+                                }
+                            });
+                        });
+                    }
+                }
+            }
+        )
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+module.exports = users;
