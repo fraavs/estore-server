dfae71eeaa1eaffb129146c7ca1f69ae718f230c
fraavs <ftcinfraavs@gmail.com>
2024-07-25 03:24:33 +0900

    connected loginform to api.

--- routes/users.js
+++ routes/users.js
@@ -88,6 +88,7 @@ users.post('/login', (req, res) => {
 
                                 res.status(200).send({
                                     token: token,
+                                    expireInSeconds: 3600
                                 });
                             } else {
                                 res.status(401).send({
